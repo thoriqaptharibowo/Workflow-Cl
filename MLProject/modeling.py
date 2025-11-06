4f5a47a98bd4f1fee6a4e98150f918091367d4cd
thoriqaptharibowo <wisma277@gmail.com>
2025-11-06 14:58:49 +0700

    first commit

---
+++ MLProject/modeling.py
@@ -0,0 +1,50 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from skopt import BayesSearchCV
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Obesity Classification")
+mlflow.sklearn.autolog()
+
+data = pd.read_csv('MLProject/preprocessed_data.csv')
+x = data.drop("NObeyesdad", axis=1)
+y = data["NObeyesdad"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
+
+def evaluate_model(model, X_test, Y_test):
+    Y_pred = model.predict(X_test)
+    results = {
+        'Confusion Matrix': confusion_matrix(Y_test, Y_pred),
+        'Accuracy': accuracy_score(Y_test, Y_pred),
+        'Precision': precision_score(Y_test, Y_pred, average='weighted'),
+        'Recall': recall_score(Y_test, Y_pred, average='weighted'),
+        'F1-Score': f1_score(Y_test, Y_pred, average='weighted')
+    }
+    return results
+
+with mlflow.start_run():
+    param_space ={
+        'n_estimators': (50, 200),
+        'max_depth': (10, 50)
+    }
+
+    model = RandomForestClassifier(random_state=42)
+    bayes_search = BayesSearchCV(estimator=model, search_spaces=param_space, n_iter=32, cv=3, n_jobs=-1, verbose=2, random_state=42)
+    rf = bayes_search.fit(x_train, y_train)
+    
+    y_pred = rf.predict(x_test)
+
+    # evaluate and log results to MLflow
+    results = evaluate_model(rf, x_test, y_test)
+
+    # Log numeric metrics to MLflow
+    for metric in ['Accuracy', 'Precision', 'Recall', 'F1-Score']:
+        if metric in results:
+            # ensure metric is a plain float
+            mlflow.log_metric(metric, float(results[metric]))
